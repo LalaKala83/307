ca1123f7578067e446f9af0bd668ec0575892daa
LalaKala83 <45587123+LalaKala83@users.noreply.github.com>
2018-12-04 13:23:27 +0100

    php - new site

--- view/user_form.php
+++ view/user_form.php
@@ -3,7 +3,7 @@
         <div data-html="true">
             <label for="username">Benutzername</label>
             <input id="username" name="username" type="text">
-            <label for="textinput">E-Mail</label>
+            <label for="email">E-Mail</label>
             <input id="email" name="email" type="email">
         </div>
     </form>
