49cb154dbad63c91d0a2ef62969d9b9bcedd7498
LalaKala83 <45587123+LalaKala83@users.noreply.github.com>
2018-12-04 10:55:47 +0100

    Test

--- lib/view.php
+++ lib/view.php
@@ -1,7 +1,41 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: balthr
  * Date: 04.12.2018
  * Time: 10:17
  */
+class View{
+    private $viewfile;
+
+    private $properties = array();
+
+    public function __construct($viewfile)
+    {
+        $this->viewfile = "./../view/$viewfile.php";
+    }
+
+    public function __set($key, $value)
+    {
+        if (!isset($this->$key)) {
+            $this->properties[$key] = $value;
+        }
+    }
+
+    public function __get($key)
+    {
+        if (isset($this->properties[$key])) {
+            return $this->properties[$key];
+        }
+    }
+
+    public function display()
+    {
+        extract($this->properties);
+
+        require './../view/header.php';
+        require $this->viewfile;
+        require './../view/footer.php';
+    }
+}
--- view/header.php
+++ view/header.php
@@ -1,2 +1,16 @@
-<?php
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title><?= $title ?></title>
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito|PT+Serif" rel="stylesheet">
+
+        <link href="/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+    </body>
+
 
