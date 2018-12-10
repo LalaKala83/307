1963b6875903c38b02a42786bb8d6f43dc935087
LalaKala83 <45587123+LalaKala83@users.noreply.github.com>
2018-12-10 08:57:02 +0100

    Menü-Anpassung -> responsives Design

--- view/header.php
+++ view/header.php
@@ -7,17 +7,26 @@
 
         <link href="https://fonts.googleapis.com/css?family=Nunito|PT+Serif" rel="stylesheet">
         <link rel="stylesheet" href="/css/style.css"  type="text/css">
+        <script src="/js/mainScript.js"></script>
 
     </head>
     <body>
-    <div id=header>
+    <div id="menueicon"><img src="/images/icon/menueicon.png" alt="Menü" onclick="showMenue()"/></div>
+    <div id=menue>
         <ul>
-            <li><a href="">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="/experience/experience/">Entdecken</a></li>
             <li><a href="/entry/search/">Suchen</a></li>
             <li><a href="/user/create">Profil</a></li>
         </ul>
-
+    </div>
+    <div id="menueLittle">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/experience/experience/">Entdecken</a></li>
+            <li><a href="/entry/search/">Suchen</a></li>
+            <li><a href="/user/create">Profil</a></li>
+        </ul>
     </div>
         <div id="content">
 
