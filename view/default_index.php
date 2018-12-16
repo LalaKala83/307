d33109f398ad683fafe989fd4cd62474784e9926
robiinioo <rnyfeler02@gmail.com>
2018-12-16 14:49:29 +0100

    design profil

--- view/default_index.php
+++ view/default_index.php
@@ -1,6 +1,6 @@
 <img class="picture" src="/images/e.jpg">
 <h1>Willkommen auf Travelgram!</h1>
-<p>Entdecken Sie die Reiseabenteuer Ihrer Freunde und anderer Blogger. Navigieren Sie auf Profil, um Ihr eigener Blog zu erstellen.</p>
+<p>Entdecken Sie die Reiseabenteuer Ihrer Freunde und anderer Blogger. Navigieren Sie auf "Profil", um Ihren eigenen Blog zu erstellen.</p>
     <img class="picture" src="/images/a.jpg">
 <p>
     <img class="picture" src="/images/f.jpg">
